b29071be86a522ef8ad50ef0146ff8c958679014
Mikhail Lavrov <lavrov.fd@gmail.com>
2023-04-23 22:18:36 +0300

    feat/del nav, add drawer instead nav

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,31 +1,61 @@
-import { Button, Drawer, Space } from 'antd';
-import { useState } from 'react';
+import { Drawer, Menu } from 'antd';
+import { HomeOutlined, BulbOutlined, FireOutlined, PictureOutlined, LineOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
-export const DrawerComponent = () => {
-  const [open, setOpen] = useState(false);
-  const showDrawer = () => {
-    setOpen(true);
-  };
-  const onClose = () => {
-    setOpen(false);
-  };
+import TELEGRAM_ICON from '../../assets/contacts/telegram.svg';
+import CALL_ICON from '../../assets/contacts/call.svg';
+import WHATSAPP_ICON from '../../assets/contacts/whatsapp.svg';
+
+const ContactIcon = ({iconSrc}) => {
   return (
-    <>
-      <Space>
-        <Button type="primary" onClick={showDrawer}>
-          Open
-        </Button>
-      </Space>
-      <Drawer
-        placement={'top'}
-        closable={false}
-        onClose={onClose}
-        open={open}
-      >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Drawer>
-    </>
+    <img 
+      width={30}
+      src={iconSrc}
+      alt="Иконка контакта"
+      style={{borderRadius: '50%', marginRight: '10px'}} />
+  )
+};
+
+const menuItems = [
+  {label: <Link to={'/'}>На главную</Link>, key: 'home', icon: <HomeOutlined />},
+  {label: <Link to={'/hot'}>Спецпредложения</Link>, key: 'hot', icon: <FireOutlined />},
+  {label:  <Link to={'/gallery'}>Галерея работ</Link>, key: 'gallery', icon: <PictureOutlined />},
+  {label: 'Услуги', key: 'services', icon: <BulbOutlined />,
+    children: [
+      {label: 'Украшение сцены', key: 'sceneDecor', icon: <LineOutlined />},
+      {label: 'Украшение помещения', key: 'roomDecor', icon: <LineOutlined />},
+    ],
+  },
+  // ==================Контакты===================
+  {label: 'Контакты', key: 'contacts', type: 'group',
+    children: [
+      {label: <a href="tel:+79315401970">Позвонить по телефону</a>, key: 'phone', icon: <ContactIcon iconSrc={CALL_ICON} />},
+      {label: <a href="https://t.me/trafik_manager_NL" target='_blank' rel="noreferrer">Написать в Telegram</a>, key: 'telegram', icon: <ContactIcon iconSrc={TELEGRAM_ICON} />},
+      {label: <a href="#s">Написать в Whatsapp</a>, key: 'whatsapp', icon: <ContactIcon iconSrc={WHATSAPP_ICON} />},
+    ],
+  },
+];
+
+export const DrawerComponent = ({ open, toggleMenu }) => {
+  const drawerOptions = {
+    placement: 'top',
+    closable: false,
+    onClose: toggleMenu,
+    open: open,
+    height: 'auto',
+    bodyStyle: {paddingLeft: 0, paddingRight: 0},
+    contentWrapperStyle: {top: '50px'},
+  };
+
+  const menuOptions = {
+    mode: 'inline',
+    items: menuItems,
+    onClick: toggleMenu,
+  };
+
+  return (
+    <Drawer {...drawerOptions}>
+      <Menu {...menuOptions}/>
+    </Drawer>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,25 +5,14 @@ import TELEGRAM_ICON from '../../assets/contacts/telegram.svg';
 import WHATSAPP_ICON from '../../assets/contacts/whatsapp.svg';
 import CALL_ICON from '../../assets/contacts/call.svg';
 import { useState } from 'react';
-import { Navigation } from '../Navigation/Navigation'; // новый компонент
+import { DrawerComponent } from '../Drawer/Drawer';
 
 const { Header } = Layout;
 
 export const HeaderComponent = () => {
   const [open, setOpen] = useState(false);
-
-  const showMenu = () => {
-    setOpen(!open);
-    !document.body.classList.contains('no-scroll') 
-      ? document.body.classList.add('no-scroll') 
-      : document.body.classList.remove('no-scroll');
-  };  
-
-  const handleMenuClose = () => {
-    setOpen(false);
-    if (document.body.classList.contains('no-scroll')) {
-      document.body.classList.remove('no-scroll');
-    }
+  const toggleMenu = () => {
+    open ? setOpen(false) : setOpen(true);
   };
 
   return (
@@ -32,21 +21,18 @@ export const HeaderComponent = () => {
         <div className={c.header__logo}>
           <a className={c.header__logoLink} href="#a">
             <img width={50} src={LOGO_ICON} alt="logo" />
-            ШАР НАШ
+            Шарики
           </a>
         </div>
         <button 
           className={`${c.header__menuButton} ${open ? c.open : ''}`}
-          onClick={showMenu} 
+          onClick={toggleMenu} 
           type='button'
           >
           <span></span>
           <span></span>
           <span></span>
         </button>
-
-        {/*  */}
-
         <div className={c.header__content}>
           <a className={c.header__contentLink} href="tel:+79315401970" title='Позвонить по телефону'>
             <img width={50} src={CALL_ICON} alt="call" />
@@ -59,7 +45,7 @@ export const HeaderComponent = () => {
             <img width={50} src={WHATSAPP_ICON} alt="WhatsApp" />
           </a>
         </div>
-        {open && <Navigation open={open} handleMenuClose={handleMenuClose} />}
+        {open && <DrawerComponent open={open} toggleMenu={toggleMenu} />}
       </div>
     </Header>
   )
