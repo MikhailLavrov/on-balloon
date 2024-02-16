a4a9594e57fabc5d8221f3ce1fb9b783423eff88
Mikhail Lavrov <lavrov.fd@gmail.com>
2024-02-16 21:01:21 +0300

    cardModal Tabs ok

--- src/components/CatalogCardModal/CatalogCardModal.jsx
+++ src/components/CatalogCardModal/CatalogCardModal.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import c from './CatalogCardModal.module.scss';
 import { Link } from 'react-router-dom';
-import { Badge, Button, Modal } from 'antd';
+import { Badge, Button, ConfigProvider, Modal, Tabs } from 'antd';
 import LOGO_IMG from '../../assets/logo.png';
-import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import { HeartOutlined, ReadOutlined, ShoppingCartOutlined, TruckOutlined } from '@ant-design/icons';
 import { personalData } from '../../data/personalData';
 import { SvgIcon } from '../SvgIcon/SvgIcon';
+import { termsData } from '../../data/termsData';
+
+const {delivery, payment} = termsData;
 
 export const CatalogCardModal = ({item, isModalOpen, setIsModalOpen}) => {
   const {category, article, title, description, price, oldPrice, image, hit, count} = item;
@@ -16,6 +19,27 @@ export const CatalogCardModal = ({item, isModalOpen, setIsModalOpen}) => {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
+
+  const tabsItems = [
+    {
+      key: 'description',
+      label: 'Описание',
+      children: description,
+      icon: <ReadOutlined />,
+    },
+    {
+      key: 'delivery',
+      label: 'Доставка',
+      children: delivery,
+      icon: <TruckOutlined />,
+    },
+    {
+      key: 'payment',
+      label: 'Оплата',
+      children: payment,
+      icon: <ShoppingCartOutlined />,
+    },
+  ];
 
   return (
     <Modal 
@@ -45,14 +69,14 @@ export const CatalogCardModal = ({item, isModalOpen, setIsModalOpen}) => {
             <p className={c.cardModal__article}>Артикул: {article}</p>
             {count>=1 ? 
               <div className={c.cardModal__inStockWrapper}><Badge status="success" /><span className={c.cardModal__inStock}>Есть в наличии</span></div> 
-            : <div className={c.cardModal__inStockWrapper}><Badge status="warning" /><span className={c.cardModal__inStock}>Скоро в продаже</span></div>}
+            : <div className={c.cardModal__inStockWrapper}><Badge status="warning" /><span className={c.cardModal__inStock}>Доступно для заказа</span></div>}
             <div className={c.cardModal__userActions}>
               <div className={c.cardModal__userActionsInner}>
                 <Button>В избранное <HeartOutlined /></Button>
                 <Button>В корзину <ShoppingCartOutlined /></Button>
               </div>
               <div className={c.cardModal__userActionsOuter}>
-                <p className={c.cardModal__userActionsOuterNotation}>Задать вопросы:</p>
+                <p className={c.cardModal__userActionsOuterNotation}>Задать вопрос:</p>
                 <div className={c.cardModal__userActionsOuterLinks}>
                   <Link target = "_blank" to={personalData.telegram}>
                     <SvgIcon icon='telegram' />
@@ -70,6 +94,26 @@ export const CatalogCardModal = ({item, isModalOpen, setIsModalOpen}) => {
               </div>
             </div>
           </div>
+        </div>
+        <div className={c.cardModal__additional}>
+        <ConfigProvider
+          theme={{
+            components: {
+              Tabs: {
+                inkBarColor: '#bebebe',
+                itemSelectedColor: '#000000',
+                itemHoverColor: '#000000',
+                itemColor: '#909090',
+              },
+            },
+          }}
+        >
+          <Tabs
+            defaultActiveKey="1"
+            centered
+            items={tabsItems}
+          />
+        </ConfigProvider>
         </div>
       </div>
     </Modal>
