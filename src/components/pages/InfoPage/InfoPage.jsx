c07443f180efc8a3a8adb7028abbe80ee7bcabc1
Mikhail Lavrov <lavrov.fd@gmail.com>
2024-02-18 17:44:54 +0300

    infoData on infoPage :)

--- src/components/pages/InfoPage/InfoPage.jsx
+++ src/components/pages/InfoPage/InfoPage.jsx
@@ -3,6 +3,7 @@ import { InfoMenu } from './InfoMenu/InfoMenu';
 import { Breadcrumb } from 'antd';
 import { HomeOutlined } from '@ant-design/icons';
 import { infoData } from '../../../data/infoData';
+import { useState } from 'react';
 
 const Breadcrumbs = () => (
   <Breadcrumb
@@ -15,20 +16,47 @@ const Breadcrumbs = () => (
         title: 'Информация для клиентов',
       },
     ]}
-    style={{fontFamily: 'Tilda Sans, Arial, sans-serif'}}
+    style={{ fontFamily: 'Tilda Sans, Arial, sans-serif' }}
   />
 );
 
 export const InfoPage = () => {
+  const [selectedTopCategory, setSelectedTopCategory] = useState('aboutcompany');
+  const [selectedCategory, setSelectedCategory] = useState('news');
+
+  const onClick = (e) => {
+    setSelectedTopCategory(e.keyPath[1]);
+    setSelectedCategory(e.key);
+  };
+
+  const infoItems = selectedCategory ? infoData[selectedTopCategory].filter(item => item.key === selectedCategory) : [];
+
   return (
-    <section className={c.infoPage} >
+    <section className={c.infoPage}>
       <div className={`${c.infoPage__container} container`}>
-      <Breadcrumbs />
-      <h1 className={c.infoPage__title}>Информация для клиентов</h1>
+        <Breadcrumbs />
+        <h1 className={c.infoPage__title}>Информация для клиентов</h1>
         <div className={c.infoPage__innerContainer}>
-          <InfoMenu />
+          <InfoMenu 
+            handleMenuClick={onClick}
+            theme={{
+              components: {
+                Menu: {
+                  itemSelectedColor: 'black',
+                  itemSelectedBg: '#cdcdcd',
+                  fontFamily: 'Tilda Sans, Arial, sans-serif',
+                },
+              },
+            }}
+          />
+          {infoItems.map((item, index) => (
+            <div key={index} className={c.infoPage__item}>
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
