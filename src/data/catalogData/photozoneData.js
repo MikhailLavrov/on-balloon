eb83320306a653de8711d68ec22eb5aefdeb87d6
Mikhail Lavrov <lavrov.fd@gmail.com>
2024-02-22 12:15:12 +0300

    add initial catalog content

--- src/data/catalogData/photozoneData.js
+++ src/data/catalogData/photozoneData.js
@@ -1,14 +1,25 @@
-import IMG_665544423 from '../../assets/catalog/payetki.webp';
+import IMG_photozone_0_901760P_1 from '../../assets/catalog/photozone/photozone_0_901760P_1.webp';
+import IMG_photozone_12_901762OP_1 from '../../assets/catalog/photozone/photozone_12_901762OP_1.webp';
 
 export const photozoneData = [
   {
     category: ['payetki'],
-    article: '66225544-423',
-    title: 'Фотозона с пайетками',
+    article: 'photozone_0_901760P_1',
+    title: 'photozone_0_901760P_1',
     description: 'Самая блестящая из всех фотозон',
     price: 11500,
-    image: IMG_665544423,
+    image: IMG_photozone_0_901760P_1,
     hit: true,
+    count: 1,
+  },
+  {
+    category: ['payetki'],
+    article: 'photozone_12_901762OP_1',
+    title: 'photozone_12_901762OP_1',
+    description: 'Самая блестящая из всех фотозон',
+    price: 11500,
+    image: IMG_photozone_12_901762OP_1,
+    hit: false,
     count: 1,
   },
 ]
