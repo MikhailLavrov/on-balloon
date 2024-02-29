23b42925e3ed05ae9fb1e32c5ae81346fff97989
Mikhail Lavrov <lavrov.fd@gmail.com>
2024-02-29 23:09:15 +0300

    feat/ orderPage fill

--- src/components/pages/OrderPage/OrderPage.jsx
+++ src/components/pages/OrderPage/OrderPage.jsx
@@ -2,7 +2,7 @@ import c from './OrderPage.module.scss';
 import { BreadcrumbsComponent } from '../../BreadcrumbsComponent/BreadcrumbsComponent';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Form, Input } from 'antd';
+import { Checkbox, Collapse, Form, Input, Radio } from 'antd';
 import ReactInputMask from 'react-input-mask';
 import { TelegramChatButton } from '../../TelegramChatButton/TelegramChatButton ';
 
@@ -12,6 +12,20 @@ export const OrderPage = () => {
   const [phoneValue, setPhoneValue] = useState('');
   const [textMessage, setTextMessage] = useState('');
   const [form] = Form.useForm();
+  const [deliveryTypeValue, setDeliveryTypeValue] = useState('delivery');
+  const deliveryOptions = [
+    {
+      label: 'Доставка',
+      value: 'delivery',
+    },
+    {
+      label: 'Самовывоз',
+      value: 'pickup',
+    },
+  ]
+  const onDeliveryTypeChange = ({ target: { value } }) => {
+    setDeliveryTypeValue(value);
+  };
 
   useEffect(() => {
     setCurrentCartItems(shoppingCartState);
@@ -28,6 +42,7 @@ export const OrderPage = () => {
       Имя: ${allValues.name}
       Телефон: ${allValues.phone}
       Email: ${allValues.email}
+      Способ доставки: ${allValues.deliveryType}
       Комментарий: ${changedValues.additional || allValues.additional || ''}
       `;
     setTextMessage(textMessage)
@@ -36,6 +51,22 @@ export const OrderPage = () => {
   const onFinish = (values) => {
     console.log('Received values of form: ', values);
   };
+
+  const cartItemsList = [
+    {
+      key: '1',
+      label: 'Список товаров',
+      children: 
+        currentCartItems.map((item, i) => {
+          return (
+            <div key={i}>
+              <img src={item.image} width={50} alt={item.title} />
+              <span>{item.title}</span> - <span>{item.count} шт.</span>
+            </div>
+          )
+        }),
+    },
+  ]
   
   return (
     <section className={c.orderPage}>
@@ -101,18 +132,62 @@ export const OrderPage = () => {
                     <Input placeholder='Email' />
                   </Form.Item>
 
+                  <Form.Item 
+                    name="deliveryType" 
+                    label={<Radio.Group options={deliveryOptions} onChange={onDeliveryTypeChange} value={deliveryTypeValue}  defaultValue={'delivery'} />}
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Заполните поле',
+                        whitespace: true,
+                      },
+                      {
+                        max: 100, 
+                        message: 'Максимум ${max} символов'
+                      },
+                    ]}
+                    >
+                    {deliveryTypeValue === 'delivery' ? <Input placeholder='Адрес доставки' /> : <span>Пункт выдачи: ЛО, г. Гатчина, ул. Киевская, д. 17кБ</span>}
+                  </Form.Item>
+
                   <Form.Item name="additional">
                     <Input.TextArea showCount maxLength={500} placeholder='Дополнительная информация' />
                   </Form.Item>
 
-                  <Form.Item >
-                    <TelegramChatButton 
-                      htmlType="submit"
-                      buttonText="Отправить"
-                      message={textMessage}
-                      />
-                  </Form.Item>
-
+              </Form.Item>
+              <Form.Item label={'Ваш заказ'} >
+                <div>
+                  <Collapse items={cartItemsList} />
+                </div>
+                <div>
+                  <p>Всего товаров: {currentCartItems.length}</p>
+                  <p>Итоговая стоимость: {}</p>
+                  <p>Тип получения: {deliveryTypeValue === 'delivery' ? 'Доставка' : 'Самовывоз'}
+                    {deliveryTypeValue === 'delivery' && <i style={{display: 'flex'}}>*Стоимость доставки уточнит менеджер после согласования заказа</i>}
+                  </p>
+                </div>
+              </Form.Item>
+              <Form.Item
+                name="agreement"
+                valuePropName="checked"
+                rules={[
+                  {
+                    validator: (_, value) =>
+                      value ? Promise.resolve() : Promise.reject(new Error('Требуется подтверждение')),
+                  },
+                ]}
+              >
+                <Checkbox>
+                  Ознакомлен с правилами обработки персональных данных
+                </Checkbox>
+              </Form.Item>
+                  
+              <Form.Item >
+                <TelegramChatButton 
+                  htmlType="submit"
+                  buttonText="Отправить"
+                  message={textMessage}
+                  />
               </Form.Item>
             </Form>
           </div>
