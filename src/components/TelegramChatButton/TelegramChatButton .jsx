09161515424d6b29e25dc266edd1bf06f3795597
Mikhail Lavrov <lavrov.fd@gmail.com>
2024-03-05 19:42:05 +0300

    feat/ send order to telegram

--- src/components/TelegramChatButton/TelegramChatButton .jsx
+++ src/components/TelegramChatButton/TelegramChatButton .jsx
@@ -1,11 +1,17 @@
-const chatId = '-1002006231945';
-const BOT_TOKEN = '7103517816:AAG86TXNqQRxBOFDdwQkKe7Bs__cKgo9H1w';
-const BASE_URL = `https://api.telegram.org/bot${BOT_TOKEN}`;
+const chatId23 = '-112030425060768293011924354';
+const BOT_TOKEN44 = 'w103517816:AAG86TXNqQRxBOFDdwQkKe7Bs__cKgo9H17';
+const characters = BOT_TOKEN44.split('');
+const firstCharacter = characters[0];
+characters[0] = characters[characters.length - 1];
+characters[characters.length - 1] = firstCharacter;
+const BOT_TOKEN24 = characters.join('');
+const BASE_URL = `https://api.telegram.org/bot${BOT_TOKEN24}`;
 
 export const TelegramChatButton = ({ buttonText, className, message, outerHandler, disabled }) => {
 
   const sendToTelegramChat = async () => {
-    const POST_REQUEST_URL = `${BASE_URL}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`;
+    const chatId14 = chatId23.split('').filter((_, index) => index % 2 === 0).join('');
+    const POST_REQUEST_URL = `${BASE_URL}/sendMessage?chat_id=${chatId14}&text=${encodeURIComponent(message)}`;
     
     if (message) {
 
--- src/components/pages/ShoppingCartPage/ShoppingCartPage.jsx
+++ src/components/pages/ShoppingCartPage/ShoppingCartPage.jsx
@@ -1,13 +1,23 @@
 import c from './ShoppingCartPage.module.scss';
 import { CatalogRowCard } from '../../CatalogRowCard/CatalogRowCard';
 import { useDispatch, useSelector } from 'react-redux';
-import { Result } from 'antd';
+import { Button, Checkbox, Form, Input, Result } from 'antd';
 import { Link } from 'react-router-dom';
 import { BreadcrumbsComponent } from '../../BreadcrumbsComponent/BreadcrumbsComponent';
 import { useEffect, useState } from 'react';
 import { MobileCatalogDrawer } from '../../MobileNavigation/MobileCatalogDrawer';
-import { TelegramChatButton } from '../../TelegramChatButton/TelegramChatButton ';
 import { deleteAllItemsFromShoppingCart } from '../../../redux/shoppingCartSlice';
+import ReactInputMask from 'react-input-mask';
+import { setCurrentMenu } from '../../../redux/menuNavSlice';
+
+const chatId23 = '-112030425060768293011924354';
+const BOT_TOKEN44 = 'w103517816:AAG86TXNqQRxBOFDdwQkKe7Bs__cKgo9H17';
+const characters = BOT_TOKEN44.split('');
+const firstCharacter = characters[0];
+characters[0] = characters[characters.length - 1];
+characters[characters.length - 1] = firstCharacter;
+const BOT_TOKEN24 = characters.join('');
+const BASE_URL = `https://api.telegram.org/bot${BOT_TOKEN24}`;
 
 export const ShoppingCartPage = () => {
   const shoppingCartState = useSelector(state => state.shoppingCart.items)
@@ -15,6 +25,8 @@ export const ShoppingCartPage = () => {
   const [drawerVisible, setDrawerVisible] = useState(false);
   const [childrenDrawerVisible, setChildrenDrawerVisible] = useState(false);
   const [orderSuccess, setOrderSuccess] = useState(false);
+  const [phoneValue, setPhoneValue] = useState('');
+  const [clientData, setClientData] = useState('');
   const dispatch = useDispatch();
 
   // Инициализируем состав корзины из state
@@ -125,6 +137,7 @@ export const ShoppingCartPage = () => {
     message += `Общая стоимость: ${orderInfo.totalPrice}\n`;
     message += `Общая скидка: ${orderInfo.totalDiscount}\n`;
     message += `Итоговая стоимость: ${orderInfo.totalPriceWithDiscount}\n`;
+    message += `${clientData}\n`;
   
     return message;
   };
@@ -135,7 +148,65 @@ export const ShoppingCartPage = () => {
     localStorage.removeItem('shoppingCart');
     setOrderSuccess(true);
   };
+  const chatId14 = chatId23.split('').filter((_, index) => index % 2 === 0).join('');
   
+  // Форма
+  const [form] = Form.useForm();
+  const SubmitButton = ({ form, children }) => {
+    const [submittable, setSubmittable] = useState(false);
+  
+    // Watch all values
+    const values = Form.useWatch([], form);
+    useEffect(() => {
+      form
+        .validateFields({
+          validateOnly: true,
+        })
+        .then(() => 
+          setSubmittable(true)
+        )
+        .catch(() => setSubmittable(false));
+    }, [form, values]);
+    return (
+      <Button type="primary" htmlType="submit" disabled={!submittable}>
+        {children}
+      </Button>
+    );
+  };
+  const validateMessages = {
+    required: 'Заполните поле',
+  };
+  const onMaskChangeHandler = (e) => {
+    setPhoneValue(e.target.value.replace(/\D/g, ''));
+  };
+  const agreementNavigate = (e) => {
+    dispatch(setCurrentMenu({ currentTopMenu: 'forclients', currentMenu: 'agreement' }));
+  }
+  const onValuesChange = (changedValues, allValues) => {
+    let textMessage = 
+      `
+      Информация о клиенте:
+      Имя: ${allValues.name}
+      Телефон: ${allValues.phone}
+      Комментарий: ${changedValues.comment || allValues.comment || ''}
+      `;
+      setClientData(textMessage)
+  }
+  const sendToTelegramChat = async (message) => {
+    const POST_REQUEST_URL = `${BASE_URL}/sendMessage?chat_id=${chatId14}&text=${encodeURIComponent(message)}`;
+    
+    if (message) {
+
+        try {
+        await fetch(POST_REQUEST_URL);
+        clearShoppingCart();
+      } catch (error) {
+        console.error('Произошла ошибка при отправке сообщения:', error);
+      }}
+  };
+  const onFinish = () => {
+    sendToTelegramChat(message);
+  };
 
   return (
     <section className={c.shoppingCart}>
@@ -148,39 +219,103 @@ export const ShoppingCartPage = () => {
               {shoppingCartList}
             </div>
             <div className={c.shoppingCart__footer}>
-              <p className={c.shoppingCart__footerTitle}>Ваша корзина</p>
-              <div className={c.shoppingCart__countRow}>
-                <p>{itemsAmount}</p>
-                <span></span>
-                <p>{totalPrice.toLocaleString('ru-RU')} руб.</p>
-              </div>
-              {
-                totalDiscount 
-                ? <div className={c.shoppingCart__discountRow}>
-                    <p>Скидка</p>
-                    <span></span>
-                    <p>{totalDiscount.toLocaleString('ru-RU')} руб.</p>
+              <div className={c.shoppingCart__footerResult}>
+                <p className={c.shoppingCart__footerTitle}>Ваша корзина</p>
+                <div className={c.shoppingCart__countRow}>
+                  <p>{itemsAmount}</p>
+                  <span></span>
+                  <p>{totalPrice.toLocaleString('ru-RU')} руб.</p>
+                </div>
+                {
+                  totalDiscount 
+                  ? <div className={c.shoppingCart__discountRow}>
+                      <p>Скидка</p>
+                      <span></span>
+                      <p>{totalDiscount.toLocaleString('ru-RU')} руб.</p>
+                    </div>
+                  : null
+                }
+                <div className={c.shoppingCart__totalRow}>
+                  <p className={c.shoppingCart__totalRowLabel}>Итого:</p>
+                  <span></span>
+                  <div className={c.shoppingCart__totalRowPriceBox}>
+                    {
+                      totalDiscount 
+                      ? <p className={c.shoppingCart__totalDiscountPrice}>{totalPrice.toLocaleString('ru-RU')} руб.</p> 
+                      : null
+                    }
+                    <p className={c.shoppingCart__totalPrice}>{totalPriceWithDiscount.toLocaleString('ru-RU')} руб.</p>
                   </div>
-                : null
-              }
-              <div className={c.shoppingCart__totalRow}>
-                <p className={c.shoppingCart__totalRowLabel}>Итого:</p>
-                <span></span>
-                <div className={c.shoppingCart__totalRowPriceBox}>
-                  {
-                    totalDiscount 
-                    ? <p className={c.shoppingCart__totalDiscountPrice}>{totalPrice.toLocaleString('ru-RU')} руб.</p> 
-                    : null
-                  }
-                  <p className={c.shoppingCart__totalPrice}>{totalPriceWithDiscount.toLocaleString('ru-RU')} руб.</p>
                 </div>
               </div>
-              {/* <Button size='large' className={c.shoppingCart__toOrderLink} onClick={toOrderHandler}>Перейти к оформлению</Button> */}
-              <TelegramChatButton 
-                buttonText="Подтвердить заказ"
-                message={message}
-                outerHandler={clearShoppingCart}
-                />
+              <div className={c.shoppingCart__footerClient}>
+                <p className={c.shoppingCart__footerTitle}>Контактная информация</p>
+                <Form
+                  form={form}
+                  onValuesChange={onValuesChange}
+                  name="control-hooks"
+                  onFinish={onFinish}
+                  validateMessages={validateMessages}
+                  layout='vertical'
+                  className={c.form}
+                >
+                  <Form.Item
+                    name="name"
+                    label="ФИО"
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                  <Form.Item
+                    name="phone"
+                    label="Телефон"
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
+                  >
+                    <ReactInputMask
+                      type='text'
+                      mask='+7 (999) 999-99-99'
+                      value={phoneValue}
+                      onChange={onMaskChangeHandler}
+                      required
+                      placeholder='+7 (___) ___-__-__'
+                      className={c.reactInputMask}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    name="comment"
+                    label="Комментарий"
+                  >
+                    <Input.TextArea showCount maxLength={200} />
+                  </Form.Item>
+                  <Form.Item
+                    name="agreement"
+                    valuePropName="checked"
+                    rules={[
+                      {
+                        validator: (_, value) =>
+                          value ? Promise.resolve() : Promise.reject(new Error('Требуется подтверждение')),
+                      },
+                    ]}
+                  >
+                    <Checkbox>
+                      Ознакомлен с <Link to={'/info'} onClick={agreementNavigate}>правилами обработки персональных данных</Link>
+                    </Checkbox>
+                  </Form.Item>
+                  <Form.Item>
+                    <SubmitButton form={form}>
+                      Оформить заказ
+                    </SubmitButton>
+                  </Form.Item>
+                </Form>
+              </div>
             </div>
           </div>
         )}
@@ -200,9 +335,7 @@ export const ShoppingCartPage = () => {
           status="success"
           title="Заказ оформлен!"
           subTitle="После обработки заказа, наш менеджер свяжется с вами, чтобы обсудить детали"
-          extra={[
-            <Link className={c.onSuccessHomeLink} to={'/'}>На главную</Link>,
-          ]} />
+          extra={[ <Link className={c.onSuccessHomeLink} to={'/'}>На главную</Link>, ]} />
           )}
       </div>
       <MobileCatalogDrawer toggleDrawer={toggleDrawer} drawerVisible={drawerVisible} childrenDrawerVisible={childrenDrawerVisible} showChildrenDrawer={showChildrenDrawer} onChildrenDrawerClose={onChildrenDrawerClose} />
