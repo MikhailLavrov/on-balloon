4d52f268b5bd97a8a405691dd1847285f211e57d
Mikhail Lavrov <lavrov.fd@gmail.com>
2024-11-07 16:49:02 +0300

    add gh-pages

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { ScrollToTop } from './utils/ScrollToTop';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/on-balloon">
       <ScrollToTop />
       <Provider store={store}>
         <App />
